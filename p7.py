db05f3cb7a4726a19605e3ba9611b57b919f0e71
qwertyn58 <86965418+qwertyn58@users.noreply.github.com>
2022-01-24 16:02:35 +0100

    Update p7.py

--- p7.py
+++ p7.py
@@ -92,8 +92,7 @@ def voisins(sk_id,a,b):
         df_voisins[df_voisins['SK_ID_CURR']==sk_id].values[0])], 
         x=a, 
         y=b,
-        color='TARGET',
-        title="A Plotly Express Figure")
+        color='TARGET')
     st.write(fig)
 
 
